4af9f2b79b1386d15904a6d461df3d37263a2225
aseem <aseem.dhawan@ypsomed.com>
2020-01-22 19:39:42 +0100

    jQuery section

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,69 @@
-$("h1").addClass("big-title margin-50");
+
+/*
+for(var i = 0; i < 5; i++){
+  document.querySelectorAll("button")[i].addEventListener("click", function(){
+    document.querySelector("h1").style.color = "purple";
+  });
+}
+
+$("button").click(function() {
+  $("h1").css("color", "purple");
+})
+
+$("input").keypress(function(event) {
+  console.log(event.key)
+})
+
+$("input").keypress(function(event) {
+  $("h1").text(event.key);
+})
+
+$(document).keypress(function(event) {
+  $("h1").text(event.key);
+})
+
+$("h1").on("mouseover", function() {
+  $("h1").css("color", "purple");
+});
+
+$("button").on("click", function() {
+  $("h1").hide();
+});
+
+$("button").on("click", function() {
+  $("h1").toggle();
+});
+
+$("button").on("click", function() {
+  $("h1").fadeOut();
+});
+
+$("button").on("click", function() {
+  $("h1").fadeIn();
+});
+
+$("button").on("click", function() {
+  $("h1").fadeToggle();
+});
+
+$("button").on("click", function() {
+  $("h1").slideUp();
+});
+
+$("button").on("click", function() {
+  $("h1").slideDown();
+});
+
+$("button").on("click", function() {
+  $("h1").slideToggle();
+});
+
+$("button").on("click", function() {
+  $("h1").animate({opacity: 0.5});
+})
+
+*/
+
+$("button").on("click", function() {
+  $("h1").slideUp().slideDown().animate({opacity: 0.5});
+})
